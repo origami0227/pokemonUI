7a0a2070d31f0b31acd73fae71769996c540d3c5
origami <1071242996@qq.com>
2022-08-30 13:16:44 +0800

    增加首页以及文档页

---
+++ src/views/home.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <div class="topnav">
+      <div class="logo"></div>
+      <div class="menu"></div>
+    </div>
+    <div class="banner">
+      <h1>宝可梦UI</h1>
+      <h2>一个简单好用的UI框架</h2>
+      <p class="actions"></p>
+      <a href="https://github.com/origami0227/pokemonUI/tree/main/src">Github</a>
+      <a href="">开始</a>
+    </div>
+  </div>
+</template>
