667e0674f46f0b748e3ffbdd5847e0cd51390c21
origami <1071242996@qq.com>
2022-08-30 10:30:11 +0800

    项目初始化

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>hi</div>
+  <Origami/>
+</template>
+
+<script>
+import Origami from './components/Origami.vue'
+
+export default {
+  name: 'App',
+  components: {
+    Origami
+  }
+}
+</script>
