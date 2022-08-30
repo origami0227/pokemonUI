abc5cdf9d57ff61ece06f8450356a774445f43c3
origami <1071242996@qq.com>
2022-08-30 14:49:32 +0800

    doc样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -2,7 +2,23 @@
   <div>
     <Topnav/>
     <div class="content">
-      <aside>边栏</aside>
+      <aside>
+        <h2>组件列表</h2>
+        <ol>
+          <li>
+            <router-link to="/doc/switch">Switch 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/button">Button 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/dialog">Dialog 组件</router-link>
+          </li>
+          <li>
+            <router-link to="/doc/tabs">Tabs 组件</router-link>
+          </li>
+        </ol>
+      </aside>
       <main>主内容</main>
     </div>
   </div>
@@ -13,3 +29,21 @@ export default {
   components: {Topnav}
 }
 </script>
+<style lang="scss" scoped>
+aside {
+  background: lightblue;
+  width: 150px;
+  padding: 16px;
+  position: fixed;
+  top: 0;
+  left: 0;
+  > h2 {
+    margin-bottom: 4px;
+  }
+  > ol {
+    > li{
+      padding: 4px 0;
+    }
+  }
+}
+</style>
