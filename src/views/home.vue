824a0f9daf8729a44fbac0e42b1e8244e2719858
origami <1071242996@qq.com>
2022-08-30 13:57:10 +0800

    update

--- src/views/home.vue
+++ src/views/home.vue
@@ -18,22 +18,4 @@ export default {
   }
 }
 </script>
-<style lang="scss" scoped>
-.topnav {
-  background: pink;
-  display: flex;
-  padding: 16px;
-  > .logo {
-    max-width: 6em;
-    margin-right: auto;
-  }
-  > .menu {
-    display: flex;
-    white-space: nowrap;
-    flex-wrap: nowrap;
-    > li {
-      margin: 0 1em;
-    }
-  }
-}
-</style>
+
