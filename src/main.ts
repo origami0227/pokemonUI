dfa7cc0292b94072799cd4a12550d2f0dd4469b2
origami <1071242996@qq.com>
2022-08-30 13:41:25 +0800

    scss更新

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import {createApp} from 'vue'
 import App from './App.vue'
-import './index.css'
+import './index.scss'
 import {createWebHashHistory, createRouter} from 'vue-router'
 import Origami from './components/Origami.vue'
 import Origami2 from './components/Origami2.vue'
