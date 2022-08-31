336bb2d575e987fac4b2f06fed3bef5aea0a5971
origami <1071242996@qq.com>
2022-08-31 10:08:02 +0800

    实现switch切换

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-
+<Switch />
   </div>
 </template>
 <script lang="ts">
--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,5 +1,49 @@
 <template>
-  <div>
-    Switch组件
-  </div>
+  <button @click="toggle" :class="{checked}">
+<!--    给button设置一个类，当这个类为ture时变化-->
+    <span></span>
+  </button>
 </template>
+<script lang="ts">
+import {ref} from 'vue'
+
+export default {
+  setup() {
+    const checked = ref(false)
+    const toggle = () => {
+      checked.value = !checked.value
+    }
+    return {checked, toggle}
+  }
+}
+</script>
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h*2;
+  border: none;
+  background: grey;
+  border-radius: $h/2;
+  position: relative;
+}
+
+span {
+  position: absolute;
+  top: 2px;
+  left: 2px;
+  height: $h2;
+  width: $h2;
+  background: white;
+  border-radius: $h2 / 2;
+}
+
+button.checked {
+  background: #00BBFF;
+}
+
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+</style>
