7598b158111f50c7f73ae33271edf9f34fbef88d
origami <1071242996@qq.com>
2022-09-01 14:13:56 +0800

    实现加载中css

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,5 +1,6 @@
 <template>
   <button class="pokemon-button" :class="classes" :disabled="disabled">
+    <span v-if="loading" class="pokemon-loadingIndicator"></span>
     <slot/>
   </button>
 </template>
@@ -23,6 +24,10 @@ export default {
     disabled: {
       type: Boolean,
       default: false,
+    },
+    loading:{
+      type:Boolean,
+      default:false
     }
   },
   setup(props) {
@@ -185,5 +190,20 @@ $grey: grey;
       color: $grey;
     }
   }
+  > .pokemon-loadingIndicator{
+    width: 14px;
+    height: 14px;
+    display: inline-block;
+    margin-right: 4px;
+    border-radius: 8px;
+    border-color: $blue $blue $blue transparent;
+    border-style: solid;
+    border-width: 2px;
+    animation: pokemon-spin 1s infinite linear;
+  }
+}
+@keyframes pokemon-spin {
+  0%{transform: rotate(0deg)}
+  100%{transform: rotate(360deg)}
 }
 </style>
