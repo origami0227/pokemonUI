c0c29597c1581405362bd527779a34233415c4c0
origami <1071242996@qq.com>
2022-09-03 15:10:08 +0800

    update css

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,25 +1,58 @@
 <template>
-  <div>
-    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
-<!--      用了v-for要加key-->
-    <component v-for="(c,index) in defaults" :key="index" :is="c"/>
+  <div class="pokemon-tabs">
+    <div class="pokemon-tabs-nav">
+      <div class="pokemon-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+    </div>
+    <div class="pokemon-tabs-content">
+      <component class="pokemon-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+    </div>
   </div>
 </template>
+
 <script lang="ts">
 import Tab from './Tab.vue'
-
 export default {
   setup(props, context) {
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
-        throw new Error('Tabs子标签必须是Tab')
+        throw new Error('Tabs 子标签必须是 Tab')
       }
     })
     const titles = defaults.map((tag) => {
       return tag.props.title
     })
-    return {defaults,titles}
+    return {
+      defaults,
+      titles
+    }
   }
 }
 </script>
+
+<style lang="scss">
+$blue: #40a9ff;
+$color: #333;
+$border-color: #d9d9d9;
+.pokemon-tabs {
+  &-nav {
+    display: flex;
+    color: $color;
+    border-bottom: 1px solid $border-color;
+    &-item {
+      padding: 8px 0;
+      margin: 0 16px;
+      cursor: pointer;
+      &:first-child {
+        margin-left: 0;
+      }
+      &.selected {
+        color: $blue;
+      }
+    }
+  }
+  &-content {
+    padding: 8px 0;
+  }
+}
+</style>
