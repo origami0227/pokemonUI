37a8858322ad4a9bd47a04af802d89a61e8896d7
origami <1071242996@qq.com>
2022-09-03 15:07:20 +0800

    渲染tabs内容

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,6 +1,25 @@
 <template>
   <div>
-    Tabs组件
-    <slot />
+    <div v-for="(t,index) in titles" :key="index">{{t}}</div>
+<!--      用了v-for要加key-->
+    <component v-for="(c,index) in defaults" :key="index" :is="c"/>
   </div>
 </template>
+<script lang="ts">
+import Tab from './Tab.vue'
+
+export default {
+  setup(props, context) {
+    const defaults = context.slots.default()
+    defaults.forEach((tag) => {
+      if (tag.type !== Tab) {
+        throw new Error('Tabs子标签必须是Tab')
+      }
+    })
+    const titles = defaults.map((tag) => {
+      return tag.props.title
+    })
+    return {defaults,titles}
+  }
+}
+</script>
