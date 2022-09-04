59593379f901ac7e5ad6c1ea82f81d61c6ad8b15
origami <1071242996@qq.com>
2022-09-04 10:37:22 +0800

    遇到vue3 bug

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,17 +1,27 @@
 <template>
   <div class="pokemon-tabs">
     <div class="pokemon-tabs-nav">
-      <div class="pokemon-tabs-nav-item" v-for="(t,index) in titles" :key="index">{{t}}</div>
+      <div class="pokemon-tabs-nav-item" @click="select(t)" :class="{selected:t===selected}" v-for="(t,index) in titles"
+           :key="index">
+        {{ t }}
+      </div>
     </div>
     <div class="pokemon-tabs-content">
-      <component class="pokemon-tabs-content-item" v-for="(c,index) in defaults" :is="c" :key="index" />
+      <component class="pokemon-tabs-content-item" :is="current"/>
     </div>
   </div>
 </template>
 
 <script lang="ts">
 import Tab from './Tab.vue'
+import {computed} from "vue";
+
 export default {
+  props: {
+    selected: {
+      type: String,
+    }
+  },
   setup(props, context) {
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
@@ -19,12 +29,23 @@ export default {
         throw new Error('Tabs 子标签必须是 Tab')
       }
     })
+    const current = computed(() => {
+      console.log(1)
+      return defaults.filter((tag) => {
+        return tag.props.title === props.selected
+      })[0]
+    })
     const titles = defaults.map((tag) => {
       return tag.props.title
     })
+    const select = (title: string) => {
+      context.emit('update:selected', title)
+    }
     return {
       defaults,
-      titles
+      titles,
+      current,
+      select
     }
   }
 }
@@ -39,18 +60,22 @@ $border-color: #d9d9d9;
     display: flex;
     color: $color;
     border-bottom: 1px solid $border-color;
+
     &-item {
       padding: 8px 0;
       margin: 0 16px;
       cursor: pointer;
+
       &:first-child {
         margin-left: 0;
       }
+
       &.selected {
         color: $blue;
       }
     }
   }
+
   &-content {
     padding: 8px 0;
   }
