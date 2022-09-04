e025ba8fbad80a42feec120c47b4a1af500f98dc
origami <1071242996@qq.com>
2022-09-04 13:53:50 +0800

    动态设置div的宽度和位置以及动画

--- src/lib/Tabs.vue
+++ src/lib/Tabs.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="pokemon-tabs">
-    <div class="pokemon-tabs-nav">
+    <div class="pokemon-tabs-nav" ref="container">
       <div class="pokemon-tabs-nav-item" @click="select(t)" :class="{selected:t===selected}" v-for="(t,index) in titles"
+           :ref="el=>{if(el)navItems[index]=el}"
            :key="index">
         {{ t }}
       </div>
+      <div class="pokemon-tabs-nav-indicator" ref="indicator"></div>
     </div>
     <div class="pokemon-tabs-content">
       <component class="pokemon-tabs-content-item" :class="{selected:c.props.title ===selected}" v-for="c in defaults"
@@ -15,7 +17,7 @@
 
 <script lang="ts">
 import Tab from './Tab.vue'
-import {computed} from "vue";
+import {computed, ref, onMounted,onUpdated} from "vue";
 
 export default {
   props: {
@@ -24,6 +26,22 @@ export default {
     }
   },
   setup(props, context) {
+    const navItems = ref<HTMLDivElement[]>([])
+    const indicator = ref<HTMLDivElement>(null)
+    const container = ref<HTMLDivElement>(null)
+    const x = ()=>{
+      const divs = navItems.value
+      const result = divs.filter(div => div.classList.contains('selected'))[0]//filter返回数组，会返回一个包含div的数组
+      // const result = divs.find(div=>div.classList.contains('selected'))
+      const {width} = result.getBoundingClientRect()//获取宽度
+      indicator.value.style.width = width + 'px'
+      const {left: left1} = container.value.getBoundingClientRect()//拿到容器container的左坐标
+      const {left: left2} = result.getBoundingClientRect()
+      const left = left2 - left1
+      indicator.value.style.left = left + 'px'
+    }
+    onMounted(x)////只在第一次渲染执行
+    onUpdated(x)
     const defaults = context.slots.default()
     defaults.forEach((tag) => {
       if (tag.type !== Tab) {
@@ -31,7 +49,6 @@ export default {
       }
     })
     const current = computed(() => {
-      console.log(1)
       return defaults.filter((tag) => {
         return tag.props.title === props.selected
       })[0]
@@ -46,7 +63,10 @@ export default {
       defaults,
       titles,
       current,
-      select
+      select,
+      navItems,
+      indicator,
+      container
     }
   }
 }
@@ -60,7 +80,9 @@ $border-color: #d9d9d9;
   &-nav {
     display: flex;
     color: $color;
+    position: relative;
     border-bottom: 1px solid $border-color;
+
 
     &-item {
       padding: 8px 0;
@@ -75,13 +97,25 @@ $border-color: #d9d9d9;
         color: $blue;
       }
     }
+
+    &-indicator {
+      position: absolute;
+      height: 3px;
+      background: $blue;
+      left: 0;
+      bottom: -1px;
+      width: 100px;
+      transition: all 250ms
+    }
   }
 
   &-content {
     padding: 8px 0;
-    &-item{
+
+    &-item {
       display: none;
-      &.selected{
+
+      &.selected {
         display: block;
       }
     }
