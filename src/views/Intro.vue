dd857087b2c416dee567b979b1ddb13aa3d2f92a
origami <1071242996@qq.com>
2022-09-08 15:13:32 +0800

    补充文档

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,5 +1,15 @@
 <template>
-  <div>
-    Intro
-  </div>
+  <article>
+    <h1>介绍</h1>
+    <p>pokemon UI 是一款基于 Vue 3 和 TypeScript 的 UI 组件库。</p>
+    <br>
+    <p>
+      这款组件库是我为了总结我近日学习的前端技术来制作的，全程亲手编写，尽量不采用第三方库，包括你现在看到的这个官网也几乎都是我自己写的。</p>
+    <br>
+    <p>所以如果强烈不建议你将此 UI 库用于生产环境。但如果你是抱着看源代码的目的来的，那么这个库还是值得一看的。源代码放在了
+      https://github.com/origami0227/pokemonUI，历史提交信息非常规范，你可以按提交的顺序逐个查看；你也可以直接查看每个组件的源代码和示例，运行方法见
+      README.md。</p>
+    <br>
+    下一节：<a href="#/doc/install">安装</a>
+  </article>
 </template>
