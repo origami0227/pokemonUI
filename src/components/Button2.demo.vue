bc5ab755a0912b68d3badba3be0d17e0f188905c
origami <1071242996@qq.com>
2022-09-10 14:59:34 +0800

    完善Button组件

---
+++ src/components/Button2.demo.vue
@@ -0,0 +1,31 @@
+<demo>
+支持 size
+</demo>
+<template>
+  <div>
+    <div>
+      <Button size="big">大号的</Button>
+      <Button>标准的</Button>
+      <Button size="small">小号的</Button>
+    </div>
+    <div>
+      <Button theme="link" size="big">大号的</Button>
+      <Button theme="link">标准的</Button>
+      <Button size="small" theme="link">小号的</Button>
+    </div>
+    <div>
+      <Button size="big" theme="text">大号的</Button>
+      <Button theme="text">标准的</Button>
+      <Button size="small" theme="text">小号的</Button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Button
+  }
+}
+</script>
---
+++ src/components/Button5.demo.vue
@@ -0,0 +1,17 @@
+<demo>
+支持显示加载中
+</demo>
+<template>
+  <div>
+    <Button loading>加载中</Button>
+  </div>
+</template>
+
+<script lang="ts">
+import Button from "../lib/Button.vue";
+export default {
+  components: {
+    Button
+  }
+}
+</script>
