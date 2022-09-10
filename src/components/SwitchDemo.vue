88bc01dd5ba1fd2b7d697801d9dbbac200069342
origami <1071242996@qq.com>
2022-09-10 13:58:41 +0800

    使用component标签优化

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -4,7 +4,7 @@
     <div class="demo">
       <h2>常规用法</h2>
       <div class="demo-component">
-        <Switch1Demo/>
+        <component :is="Switch1Demo"/>
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -16,7 +16,7 @@
     <div class="demo">
       <h2>支持 disabled </h2>
       <div class="demo-component">
-        <Switch2Demo/>
+        <component :is="Switch2Demo"/>
       </div>
       <div class="demo-actions">
         <Button>查看代码</Button>
@@ -29,7 +29,6 @@
 </template>
 
 <script lang="ts">
-import Switch from '../lib/Switch.vue'
 import Button from '../lib/Button.vue'
 import Switch1Demo from '../components/Switch1.demo.vue'
 import Switch2Demo from '../components/Switch2.demo.vue'
@@ -39,10 +38,7 @@ import {
 
 export default {
   components: {
-    Switch,
     Button,
-    Switch1Demo,
-    Switch2Demo
   },
   setup() {
     const bool = ref(false)
