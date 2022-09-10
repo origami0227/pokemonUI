037b1500349bd196122d2734e81c53555517af9f
origami <1071242996@qq.com>
2022-09-10 14:16:56 +0800

    引入prismjs增加高亮效果

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -10,7 +10,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{Switch1Demo.__sourceCode}}</pre>
+        <pre class="language-html" v-html="Prism.highlight(Switch1Demo.__sourceCode,Prism.languages.html,'html')"></pre>
       </div>
     </div>
     <div class="demo">
@@ -22,7 +22,7 @@
         <Button>查看代码</Button>
       </div>
       <div class="demo-code">
-        <pre>{{Switch2Demo.__sourceCode}}</pre>
+        <pre class="language-html" v-html="Prism.highlight(Switch2Demo.__sourceCode,Prism.languages.html,'html')"></pre>
       </div>
     </div>
   </div>
@@ -32,6 +32,9 @@
 import Button from '../lib/Button.vue'
 import Switch1Demo from '../components/Switch1.demo.vue'
 import Switch2Demo from '../components/Switch2.demo.vue'
+import 'prismjs'
+import 'prismjs/themes/prism-okaidia.css'
+const Prism = (window as any).Prism//骗过ts
 import {
   ref
 } from 'vue'
@@ -43,7 +46,7 @@ export default {
   setup() {
     const bool = ref(false)
     return {
-      bool, Switch2Demo, Switch1Demo
+      bool, Switch2Demo, Switch1Demo,Prism
     }
   }
 }
