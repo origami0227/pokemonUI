998e8c63a241ab11d72fdf3a6986447a0b86c405
origami <1071242996@qq.com>
2022-09-10 14:41:07 +0800

    删掉冗余代码

--- src/components/SwitchDemo.vue
+++ src/components/SwitchDemo.vue
@@ -7,23 +7,18 @@
 </template>
 
 <script lang="ts">
-import Button from '../lib/Button.vue'
 import Switch1Demo from '../components/Switch1.demo.vue'
 import Switch2Demo from '../components/Switch2.demo.vue'
 import Demo from '../components/Demo.vue'
-import {
-  ref
-} from 'vue'
+
 
 export default {
   components: {
-    Button,
     Demo
   },
   setup() {
-    const bool = ref(false)
     return {
-      bool, Switch2Demo, Switch1Demo,
+       Switch2Demo, Switch1Demo,
     }
   }
 }
