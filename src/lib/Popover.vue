a3047e877180b9022c4dac2c2938fbb4e0bc2b14
origami <1071242996@qq.com>
2022-09-17 15:07:20 +0800

    修复潜在bug完善popover

--- src/lib/Popover.vue
+++ src/lib/Popover.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="popover" @click="show">
-    <div ref="contentWrapper" class="content-wrapper" v-if="visible" @click.stop>
+  <div class="popover" @click="onClick" ref="popover">
+    <div ref="contentWrapper" class="content-wrapper" v-if="visible">
       <slot name="content"></slot>
     </div>
     <span ref="triggerWrapper">
@@ -17,30 +17,44 @@ export default {
     return {visible: false}
   },
   methods: {
-    show() {
-      this.visible = !this.visible
-      if (this.visible === true) {
-        setTimeout(() => {
-          document.body.appendChild(this.$refs.contentWrapper)
-          let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
-          this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
-          this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+    positionContent() {
+      document.body.appendChild(this.$refs.contentWrapper)
+      let {width, height, top, left} = this.$refs.triggerWrapper.getBoundingClientRect()
+      this.$refs.contentWrapper.style.left = left + window.scrollX + 'px'
+      this.$refs.contentWrapper.style.top = top + window.scrollY + 'px'
+    },
+    onClickDocument(e){
+      if (this.$refs.popover &&
+          (
+              this.$refs.popover === e.target ||
+              this.$refs.popover.contains(e.target))
+      ) {
+        return
+      }
+      this.close()
+    },
 
-          let eventHandler = () => {
-            this.visible = false
-            console.log('document隐藏popover')
-            document.removeEventListener('click', eventHandler)
-          }
-          document.addEventListener('click', eventHandler)
-        }, 0)
-      } else {
-        console.log('vm隐藏popover')
+    open() {
+      this.visible = true
+      setTimeout(() => {
+        this.positionContent()
+        document.addEventListener('click', this.onClickDocument)
+      }, 0)
+    },
+    close() {
+      this.visible = false
+      document.removeEventListener('click',this. onClickDocument)
+    },
+    onClick(event) {
+      if (this.$refs.triggerWrapper.contains(event.target)) {
+        if (this.visible === true) {
+          this.close()
+        } else {
+          this.open()
+        }
       }
     }
   },
-  mounted() {
-    console.log(this.$refs.triggerWrapper)
-  }
 }
 </script>
 <style lang="scss" scoped>
