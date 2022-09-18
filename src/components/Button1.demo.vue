51aa899b10df3a444ce8431363330b5440c9b03c
origami <1071242996@qq.com>
2022-09-18 13:51:10 +0800

    更新button样式和demo样式

--- src/components/Button1.demo.vue
+++ src/components/Button1.demo.vue
@@ -1,10 +1,13 @@
 <demo>
-常规使用
+常规使用(三种主题)
 </demo>
 <template>
   <div>
     <Button>你好</Button>
-    <Button theme="link">你好</Button>
+    <Button style="background: #e89ec5">你好</Button>
+    <Button style="background: #cbe89e">你好</Button>
+    <Button theme="link" >你好</Button>
+    <Button theme="link" style="background: #e8d59e">你好</Button>
     <Button theme="text">你好</Button>
   </div>
 </template>
--- src/components/Demo2.vue
+++ src/components/Demo2.vue
@@ -40,6 +40,7 @@ $border-color: #d9d9d9;
 .demo {
   border: 1px solid $border-color;
   margin: 16px 0 32px;
+  background-color: #fafbfb;
 
   > h2 {
     font-size: 20px;
